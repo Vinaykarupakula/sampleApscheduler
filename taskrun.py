b748f33711fbb0352ac11b2c77e631ac9bcd7522
VinayKumar.karupakula <vkarupakula.k@spurtreetech.com>
2022-10-14 11:55:50 +0530

    Adding code

---
+++ taskrun.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from sched import scheduler
+import time
+from time import time
+from apscheduler.schedulers.background import BackgroundScheduler, BlockingScheduler
+
+
+def myjob():
+    print("Hello World", datetime.now())
+
+def myjob2():
+    print("this is another job named job2", datetime.now())
+
+
+scheduler = BlockingScheduler()
+# scheduler = BackgroundScheduler()
+scheduler.add_job(func=myjob, trigger='interval', seconds=5, id = 'my custom task')
+
+
+
+scheduler.add_job(func=myjob2, trigger='interval', seconds = 6, id = 'my custom task 2')
+scheduler.start()
+time.sleep(2)
+scheduler.start()
+
+
+
+
+
+
